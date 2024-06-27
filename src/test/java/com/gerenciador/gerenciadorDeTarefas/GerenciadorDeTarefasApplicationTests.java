f32904053e54f758a85c4e3cbae2f6dbb88d19ae
marceloflp <mfelipedasilva2020@gmail.com>
2024-06-27 14:21:55 -0300

    Criação do Projeto

---
+++ src/test/java/com/gerenciador/gerenciadorDeTarefas/GerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.gerenciadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
