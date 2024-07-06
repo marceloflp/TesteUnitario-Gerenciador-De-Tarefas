cf7321ef129bea6d4c27792848f84fea734921fc
marceloflp <mfelipedasilva2020@gmail.com>
2024-07-06 19:27:19 -0300

    Finalizando UsuarioController e ProjetoController

--- src/main/java/com/gerenciador/gerenciadorDeTarefas/controllers/ProjetoController.java
+++ src/main/java/com/gerenciador/gerenciadorDeTarefas/controllers/ProjetoController.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,19 @@ public class ProjetoController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deletarProjeto(@PathVariable Long id) {
+		service.deletarProjeto(id);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Projeto> atualizarUsuario(@PathVariable Long id, @RequestBody Projeto proj) {
+		Projeto projeto = service.atualizarProjeto(id, proj);
+		
+		return ResponseEntity.ok().body(projeto);
 	}
 }
--- src/main/java/com/gerenciador/gerenciadorDeTarefas/controllers/UsuarioController.java
+++ src/main/java/com/gerenciador/gerenciadorDeTarefas/controllers/UsuarioController.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,25 +24,38 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService service;
-	
+
 	@GetMapping()
-	public ResponseEntity<List<Usuario>> listarUsuarios(){
+	public ResponseEntity<List<Usuario>> listarUsuarios() {
 		List<Usuario> usuarios = service.listarUsuarios();
-		
+
 		return ResponseEntity.ok().body(usuarios);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Usuario> usuarioId(@PathVariable Long id){
+	public ResponseEntity<Usuario> usuarioId(@PathVariable Long id) {
 		Usuario obj = service.buscarPorId(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@PostMapping()
-	public ResponseEntity<Usuario> inserirUsuario(@RequestBody Usuario obj){
+	public ResponseEntity<Usuario> inserirUsuario(@RequestBody Usuario obj) {
 		obj = service.salvarUsuario(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+		service.deletarUsuario(id);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping()
+	public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario obj) {
+		Usuario user = service.atualizarUsuario(id, obj);
+		
+		return ResponseEntity.ok().body(user);
 	}
 }
