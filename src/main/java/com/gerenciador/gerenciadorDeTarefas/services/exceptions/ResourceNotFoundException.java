8c952873ceb37d95f837abf18a33056ac0d1e54a
marceloflp <mfelipedasilva2020@gmail.com>
2024-07-11 15:29:03 -0300

    Tratamento de exceções I - ResourceNotFoundExeption

---
+++ src/main/java/com/gerenciador/gerenciadorDeTarefas/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.gerenciador.gerenciadorDeTarefas.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException() {
+		super("Recurso não encontrado.");
+	}
+
+}
