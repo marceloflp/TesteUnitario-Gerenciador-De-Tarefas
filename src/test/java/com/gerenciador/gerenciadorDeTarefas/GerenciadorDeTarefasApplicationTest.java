d7e260cb5da5b3621e36de930ff9b373a836ff7a
marceloflp <mfelipedasilva2020@gmail.com>
2024-09-12 16:00:42 -0300

    Criação do projeto + teste na classe main

---
+++ src/test/java/com/gerenciador/gerenciadorDeTarefas/GerenciadorDeTarefasApplicationTest.java
@@ -0,0 +1,12 @@
+package com.gerenciador.gerenciadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+
+class GerenciadorDeTarefasApplicationTest {
+
+	@Test
+	void main() {
+		GerenciadorDeTarefasApplication.main(new String[] {});
+	}
+
+}
